59614754fd51c842f6b948d0123de2b4aaa576f6
kn1kn1 <k71kn1@gmail.com>
2013-05-17 22:33:18 +0900

    Support only Ubuntu 12.04

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ description      'Installs/Configures supercollider'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 depends          'apt'
-supports         'ubuntu'
+supports         'ubuntu', '= 12.04'
